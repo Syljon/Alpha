5496ce8fc74d462cc814ab5caa9a83dcbd2c09ed
Syljon <syljon2@gmail.com>
2018-09-08 14:40:05 +0200

    Revert "Revert "fix add employee and reset form""
    This reverts commit 8331674055d85936a3307a4d77069b0f465fc4f9.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { WorkersListComponent } from './workers-list/workers-list.component';
 import { WorkerFormComponent } from './worker-form/worker-form.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +20,11 @@ import { WelcomePageComponent } from './welcome-page/welcome-page.component';
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    RouterModule.forRoot([{path:'welcome', component: WelcomePageComponent},
+      {path:'employee-list', component: WorkersListComponent},
+      {path: 'employee-form', component: WorkerFormComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
