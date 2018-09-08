1a1d9685abd528d91e7e4ef91f0e005ff03f54cf
Syljon <syljon2@gmail.com>
2018-09-08 12:14:36 +0200

    create welcom-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { WorkersListComponent } from './workers-list/workers-list.component';
 import { WorkerFormComponent } from './worker-form/worker-form.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 @NgModule({
   declarations: [
     AppComponent,
     WorkersListComponent,
-    WorkerFormComponent
+    WorkerFormComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
